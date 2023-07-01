414286cba976444ed6f8faef4a2e7e494b5769d4
Roman Diaz <roman.diaz09@gmail.com>
2023-07-01 01:23:28 -0300

    Cambio de íconos Dark Mode y estilos footer

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { routes } from '../Routes/utils/routes'
 import { useDentistsContext } from './utils/global.context';
-import ToggleOnIcon from '@mui/icons-material/ToggleOn';
-import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+import { DarkMode, DarkModeOutlined } from '@mui/icons-material';
 
 const Navbar = () => {
 
@@ -15,10 +14,9 @@ const Navbar = () => {
         <Link to={routes.home}><li>Home</li></Link>
         <Link to={routes.contact}><li>Contact</li></Link>
         <Link to={routes.favorites}><li>Favorites</li></Link>
+        {dentistState.theme == 'light' && <Link><DarkMode style={{ padding: "0rem 1.5rem"}} onClick={() => dentistDispatch({type: 'CHANGE_THEME', payload: 'dark'})}></DarkMode></Link>}
+      {dentistState.theme == 'dark' && <Link><DarkModeOutlined style={{ padding: "0rem 1.5rem"}} onClick={() => dentistDispatch({type: 'CHANGE_THEME', payload: 'light'})}></DarkModeOutlined></Link>}
       </ul>
-      <label>Dark Mode</label>
-      {dentistState.theme == 'light' && <ToggleOffIcon style={{ padding: "0rem 1.5rem"}} onClick={() => dentistDispatch({type: 'CHANGE_THEME', payload: 'dark'})}></ToggleOffIcon>}
-      {dentistState.theme == 'dark' && <ToggleOnIcon style={{ padding: "0rem 1.5rem"}} onClick={() => dentistDispatch({type: 'CHANGE_THEME', payload: 'light'})}></ToggleOnIcon>}
     </nav>
   )
 }
