32be53bd714d2a56a9f014a3e01170e29a4d4314
Roman Diaz <roman.diaz09@gmail.com>
2023-07-01 01:51:51 -0300

    Cambiada la ubicación de Clean Favorites

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -9,15 +9,13 @@ const Favs = () => {
   const { dentistState, dentistDispatch } = useDentistsContext();
 
   return (
-    <>
-    <Button onClick={() => dentistDispatch({type: 'CLEAN_FAVORITES'})} sx={dentistState.theme == 'dark' ? muiStyles.dark.button : muiStyles.light.button}>Clean Favorites</Button>
     <div className={dentistState.theme == 'dark' ? "dark" : ""} style={{padding: "1.5rem"}}>
+    <Button onClick={() => dentistDispatch({type: 'CLEAN_FAVORITES'})} sx={dentistState.theme == 'dark' ? muiStyles.dark.button : muiStyles.light.button}>Clean Favorites</Button>
       <h1>Dentists Favorites</h1>
       <div className="card-grid">
         {dentistState.favorites.length == 0 ? <h2>You have no dentists in your favorites</h2> : dentistState.favorites.map((fav) => <Card key={fav.id} name={fav.name} username={fav.username} id={fav.id}/>)}
       </div>
     </div>
-    </>
   );
 };
 
